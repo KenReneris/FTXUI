70390e0f3a3fac55c6531e1c79af0f21d42c645c
KenReneris <KenReneris@users.noreply.github.com>
2025-06-28 16:34:34 +0000

    deploy: 2d5026973010102deabdcc8a9902e0bbc09aee85

---
+++ box__helper_8cpp.js
@@ -0,0 +1,4 @@
+var box__helper_8cpp =
+[
+    [ "Compute", "box__helper_8cpp.html#a5aae26b177ae86400ad4392b3583d8a4", null ]
+];
---
+++ box__helper_8hpp.js
@@ -0,0 +1,5 @@
+var box__helper_8hpp =
+[
+    [ "Element", "structftxui_1_1box__helper_1_1Element.html", "structftxui_1_1box__helper_1_1Element" ],
+    [ "Compute", "box__helper_8hpp.html#a5aae26b177ae86400ad4392b3583d8a4", null ]
+];
